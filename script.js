4533a3a4b1a2b07eff4bc1da8eb7a2311f1b5bb0
Michael Ream <mrmream@gmail.com>
2023-01-27 14:30:49 -0700

    Add play function for 1 round

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ function getComputerChoice(choices) {
     let randomIndex = Math.floor(Math.random()*choices.length)
     return choices[randomIndex]
 }
-console.log(getComputerChoice(RPS))
 
 
+const playerSelection = ("rock")
+const computerSelection = getComputerChoice(RPS)
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "You Win! rock beats scissors"}
+        else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "You Lose! paper beats rock"}
+        else if (playerSelection === "rock" && computerSelection === "rock"){
+        return "Tie game"}
+}
+
+console.log(playRound(playerSelection, computerSelection))
