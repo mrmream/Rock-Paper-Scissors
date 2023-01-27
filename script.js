f8cd1f645c2f101c06061078c187c123dda75d56
Michael Ream <mrmream@gmail.com>
2023-01-27 14:54:10 -0700

    Add prompt, all available options (RPS), can play 5 rounds

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getComputerChoice(choices) {
 }
 
 
-const playerSelection = ("rock")
+const playerSelection = prompt("Please select rock, paper, or scissors:").toLowerCase()
 const computerSelection = getComputerChoice(RPS)
 
 function playRound(playerSelection, computerSelection){
@@ -15,6 +15,39 @@ function playRound(playerSelection, computerSelection){
         return "You Lose! paper beats rock"}
         else if (playerSelection === "rock" && computerSelection === "rock"){
         return "Tie game"}
+        else if (playerSelection === "paper" && computerSelection === "rock") {
+            return "You Win! paper beats rock"
+            } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            return "You Lose! scissors beats paper"
+            } else if (playerSelection === "paper" && computerSelection === "paper") {
+            return "Tie game"
+            } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            return "You Lose! rock beats scissors"
+            } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            return "You Win! scissors beats paper"
+            } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+            return "Tie game"
+            }
+        
 }
 
-console.log(playRound(playerSelection, computerSelection))
+
+
+let rounds = 0;
+let playerWins = 0;
+let computerWins = 0;
+while (rounds < 5) {
+    const playerSelection = prompt("Please select rock, paper, or scissors:").toLowerCase();
+    const computerSelection = getComputerChoice(RPS);
+    const result = playRound(playerSelection, computerSelection);
+    if (result.startsWith("You Win")) {
+        playerWins++;
+    } else if (result.startsWith("You Lose")) {
+        computerWins++;
+    } else if (result.startsWith("Tie")) {
+        rounds--
+    }
+    console.log(result);
+    rounds++;
+}
+console.log(`Player wins: ${playerWins} | Computer wins: ${computerWins}`);
