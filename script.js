5418810dd32e580609a278ad6319729679ab22fb
Michael Ream <mrmream@gmail.com>
2023-01-31 01:46:53 -0700

    Counts rounds, has winner/loser message after 5 rounds

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function getComputerChoice(choices) {
     return choices[randomIndex]
 }
 
-const computerSelection = getComputerChoice(RPS)
-
 function playRound(playerSelection, computerSelection){
     if (playerSelection === "rock" && computerSelection === "scissors"){
         return "You Win! rock beats scissors"}
@@ -14,37 +12,91 @@ function playRound(playerSelection, computerSelection){
         else if (playerSelection === "rock" && computerSelection === "rock"){
         return "Tie game"}
         else if (playerSelection === "paper" && computerSelection === "rock") {
-            return "You Win! paper beats rock"
-            } else if (playerSelection === "paper" && computerSelection === "scissors") {
-            return "You Lose! scissors beats paper"
-            } else if (playerSelection === "paper" && computerSelection === "paper") {
-            return "Tie game"
-            } else if (playerSelection === "scissors" && computerSelection === "rock") {
-            return "You Lose! rock beats scissors"
-            } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            return "You Win! scissors beats paper"
-            } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-            return "Tie game"
-            } else 
-            return "Invalid"
+        return "You Win! paper beats rock"
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! scissors beats paper"
+        } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "Tie game"
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! rock beats scissors"
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! scissors beats paper"
+        } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        return "Tie game"
+        } else 
+        return "Invalid"
         
 }
 
+let playerWins = 0;
+let computerWins = 0;
+let totalRounds = 0;
+
+const main = document.querySelector("div")
 const rockButton = document.querySelector("#rock")
 const paperButton = document.querySelector("#paper")
 const scissorsButton = document.querySelector("#scissors")
 
+const scores = document.createElement("p")
+scores.classList.add('scores')
+main.appendChild(scores)
+
+const checkForWinner = function() {
+    if (totalRounds === 5) {
+      if (playerWins > computerWins) {
+        alert("You are a Winner!");
+      } else if (computerWins > playerWins) {
+        alert("You are a Loser!");
+      }
+    }
+  };
+
 rockButton.addEventListener("click", function() {
+    const computerSelection = getComputerChoice(RPS)
     const result = playRound("rock", computerSelection);
+    if (result.startsWith("You Win")) {
+        playerWins++;
+    } else if (result.startsWith("You Lose")) {
+        computerWins++;
+    } else if (result.startsWith("Tie")){
+        totalRounds--
+    }
+    
+    totalRounds++
+    checkForWinner()
+    scores.innerHTML = `Player wins
+: ${playerWins} | Computer wins: ${computerWins}`;
     alert(result);
-  });
-  
-  paperButton.addEventListener("click", function() {
+});
+
+paperButton.addEventListener("click", function() {
+    const computerSelection = getComputerChoice(RPS)
     const result = playRound("paper", computerSelection);
+    if (result.startsWith("You Win")) {
+        playerWins++;
+    } else if (result.startsWith("You Lose")) {
+        computerWins++;
+    } else if (result.startsWith("Tie")){
+        totalRounds--
+    }
+    totalRounds++
+    checkForWinner()
+    scores.innerHTML = `Player wins: ${playerWins} | Computer wins: ${computerWins}`;
     alert(result);
-  });
-  
-  scissorsButton.addEventListener("click", function() {
+});
+
+scissorsButton.addEventListener("click", function() {
+    const computerSelection = getComputerChoice(RPS)
     const result = playRound("scissors", computerSelection);
+    if (result.startsWith("You Win")) {
+        playerWins++;
+    } else if (result.startsWith("You Lose")) {
+        computerWins++;
+    } else if (result.startsWith("Tie")){
+        totalRounds--
+    }
+    totalRounds++
+    checkForWinner()
+    scores.innerHTML = `Player wins: ${playerWins} | Computer wins: ${computerWins}`;
     alert(result);
-  });
+});
